c90fca3e3052d13bc7b573aae3f786b7b2676fd6
Asif Ali <aali9548702@gmail.com>
2025-05-31 17:03:39 +0530

    feat: enhance user model with validation messages and password hashing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,23 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, "Username is required"],
         unique: true,
+        trim: true,
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required"],
         unique: true,
+        trim: true,
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Password is required"],
+        trim: true,
     },
     avatar: {
         type: String,
@@ -21,3 +25,15 @@ const userSchema = new Schema({
 }, { timestamps: true });
 
 export const User = mongoose.model("User", userSchema);
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(5);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
