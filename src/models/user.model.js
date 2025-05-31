df92948ce9fa14cd00597623da2efb94ce9aec31
Asif Ali <aali9548702@gmail.com>
2025-05-31 17:19:55 +0530

    feat: add refreshToken field and token generation methods to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
@@ -22,18 +23,47 @@ const userSchema = new Schema({
     avatar: {
         type: String,
     },
+    refreshToken: {
+        type: String,
+    },
 }, { timestamps: true });
 
-export const User = mongoose.model("User", userSchema);
-
+// pre-save hook
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-
+    
     const salt = await bcrypt.genSalt(5);
     this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
+// methods
 userSchema.methods.comparePassword = async function(password){
     return await bcrypt.compare(password, this.password);
 }
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    })
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    })
+}
+
+export const User = mongoose.model("User", userSchema);
+
+
