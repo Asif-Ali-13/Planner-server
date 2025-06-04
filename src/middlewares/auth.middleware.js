d6ef54e42a1a397d7b618e3ca4609d14983ebe76
Asif Ali <aali9548702@gmail.com>
2025-06-04 23:11:53 +0530

    fix: update error handling in JWT verification to use correct status codes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        if(!token) throw new ApiError(401, "Invalid token");
+        if(!token) throw new ApiError(400, "Invalid token");
         
         const decodedToken =  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
@@ -15,6 +15,6 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         req.user = user;
         next();
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid token");
+        throw new ApiError(400, error?.message || "Invalid token");
     }
 });
