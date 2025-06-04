d9172381ee059dd6bed50d999e107c8eb341c0e9
Asif Ali <aali9548702@gmail.com>
2025-06-04 20:31:12 +0530

    fix: ensure tokens are unset before user logout response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -62,8 +62,17 @@ export const getCurrentUser = asyncHandler(async (req, res) => {
 });
 
 export const signOutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        { $unset: { refreshToken: 1 , accessToken: 1 } },
+        { new: true }
+    );
+
     const options = { httpOnly: true, secure: true };
-    res.clearCookie("accessToken", options);
-    res.clearCookie("refreshToken", options);
-    return res.status(200).json(new ApiResponse(200, null, "User loggedOut successfully"));
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, null, "User loggedOut successfully"));
 });
