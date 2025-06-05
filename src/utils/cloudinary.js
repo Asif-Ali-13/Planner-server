7a9c2957d8f28caf2a2df77bd70b3d084157fe5d
Asif Ali <aali9548702@gmail.com>
2025-06-05 13:21:38 +0530

    feat: add cloudinary utility functions for file upload and deletion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // upload file on cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully 
+        fs.unlinkSync(localFilePath);
+        return uploadResponse;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally saved temp files 
+    }
+}
+
+export const deleteOnCloudinary = async (publicId) => {
+    try{
+        if(!publicId) return null;
+        
+        const deleteResponse = await cloudinary.uploader.destroy(publicId, {
+            resource_type: "auto"
+        });
+        return deleteResponse;
+    } catch (error) {
+        return error;
+    }
+}
