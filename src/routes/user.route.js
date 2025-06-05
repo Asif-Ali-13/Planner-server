1548ca843d0b180f017802e42df0dc96f765bdbb
Asif Ali <aali9548702@gmail.com>
2025-06-05 16:51:27 +0530

    feat: add updateAvatar route with multer middleware for avatar uploads

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,22 @@
 import { Router } from 'express';
-import { User } from '../models/user.model.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
-import { getCurrentUser, signInUser, signOutUser, signUpUser } from '../controllers/user.controller.js';
+import { upload } from '../middlewares/multer.middleware.js';
+import { 
+    getCurrentUser, 
+    signInUser, 
+    signOutUser, 
+    signUpUser, 
+    updateUserAvatar
+} from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.route("/signup").post(signUpUser);
 router.route("/signin").post(signInUser);
-router.route("/signout").post(signOutUser);
 
 router.use(verifyJWT);
+router.route("/signout").post(signOutUser);
 router.route("/me").get(getCurrentUser);
+router.route("/updateAvatar").patch(upload.single("avatar"), updateUserAvatar);
 
 export const userRoutes = router;
